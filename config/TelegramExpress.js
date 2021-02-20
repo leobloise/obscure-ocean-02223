83f5faad24342b92ceb7b41cbb5cb14d7275075f
Leonardo C. S. Bloise <leonardo.bloise@outlook.com>
2021-02-20 15:15:59 -0300

    FInalizando e arrumando

--- config/TelegramExpress.js
+++ config/TelegramExpress.js
@@ -13,22 +13,35 @@ class Bot {
 
     initialize() {
 
-        this.bot.addListener('message', message => {
+        this.bot.on(/\/link/, message => {
+            this._basic(message)
+        })
 
-            if('from' in message) {
-                return this.bot.sendMessage(message.from.id,  process.env.MSG_PRIVATE || "Entre no novo grupo: https://t.me/joinchat/G3kYnKSmt5LVII7J")
-                .then(res => true)
-                .catch(err => {
-                    console.log(`User ${message.from.id} does not initiate a chat with me`)
-                    this._sendMessage(message, process.env.MSG_PUBLIC || "Entre no novo grupo: https://t.me/joinchat/G3kYnKSmt5LVII7J")
-                    return true;
-                })    
-            }
-                
-            return this._sendMessage(message, "Olá! Eu fui feito para atuar em um grupo! Logo, adicione-me lá que irei atuar como desejado")            
+        this.bot.addListener('message', message => { 
+            this._basic(message)
         })                                              
     }
+
+    _basic(message) {
+
+        if('from' in message) {
+            return this._sendPrivateBasicMessageFromGroup(message);
+        }
+            
+        return this._sendMessage(message, "Olá! Eu fui feito para atuar em um grupo! Logo, adicione-me lá que irei atuar como desejado")            
     
+    }
+    
+    _sendPrivateBasicMessageFromGroup(message) {
+        return this.bot.sendMessage(message.from.id,  process.env.MSG_PRIVATE || "Entre no novo grupo: https://t.me/joinchat/G3kYnKSmt5LVII7J")
+        .then(res => true)
+        .catch(err => {
+            console.log(`User ${message.from.id} does not initiate a chat with me`)
+            this._sendMessage(message, process.env.MSG_PUBLIC || "Entre no novo grupo: https://t.me/joinchat/G3kYnKSmt5LVII7J")
+            return true;
+        }) 
+    }
+
     _sendMessage(messageTelegram, message) {
 
         return new Promise((resolve, reject) => {
